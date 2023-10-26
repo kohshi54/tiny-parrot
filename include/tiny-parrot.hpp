7be6b2fc2e8004a55e18048cbfaff25881ae1c59
kohshi54 <kohshi54.yam@gmail.com>
2023-10-27 03:21:08 +0900

    read instructions.

--- include/tiny-parrot.hpp
+++ include/tiny-parrot.hpp
@@ -2,5 +2,10 @@
 # define TINY_PARROT_HPP
 
 # include <iostream>
+# include <fstream>
+# include <string>
+# include <sstream>
+# include <algorithm>
+
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,28 @@
 #include "tiny-parrot.hpp"
 
-int main(void)
+int main(int argc, char *argv[])
 {
+    if (argc != 2)
+    {
+        std::cout << "usage: tiny-parrot [file]" << std::endl;
+        return (1);
+    }
     std::cout << "hello from RV32I!" << std::endl;
+
+    std::ifstream file(argv[1]);
+    std::string instr;
+    while (std::getline(file, instr))
+    {
+        std::cout << instr << std::endl;
+        std::string opcode = instr.substr(0, instr.find(" "));
+        std::cout << opcode << std::endl;
+        std::stringstream operands = static_cast<std::stringstream>(&instr[instr.find(" ")]);
+        std::string opr;
+        while (getline(operands, opr, ','))
+        {
+            opr.erase(std::remove(opr.begin(), opr.end(), ' '), opr.end());
+            std::cout << opr << std::endl;
+        }
+        std::cout << "--------------" << std::endl;
+    }
 }
