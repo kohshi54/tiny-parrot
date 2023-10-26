5fc5e27ae228e5c556b65e7e22c98a39b0432cb1
kohshi54 <kohshi54.yam@gmail.com>
2023-10-27 03:59:33 +0900

    add resisters.

--- include/tiny-parrot.hpp
+++ include/tiny-parrot.hpp
@@ -7,5 +7,40 @@
 # include <sstream>
 # include <algorithm>
 
+typedef struct s_resister
+{
+    const static uint32_t   x0 = 0;
+    static uint32_t         x1;
+    static uint32_t         x2;
+    static uint32_t         x3;
+    static uint32_t         x4;
+    static uint32_t         x5;
+    static uint32_t         x6;
+    static uint32_t         x7;
+    static uint32_t         x8;
+    static uint32_t         x9;
+    static uint32_t         x10;
+    static uint32_t         x11;
+    static uint32_t         x12;
+    static uint32_t         x13;
+    static uint32_t         x14;
+    static uint32_t         x15;
+    static uint32_t         x16;
+    static uint32_t         x17;
+    static uint32_t         x18;
+    static uint32_t         x19;
+    static uint32_t         x20;
+    static uint32_t         x21;
+    static uint32_t         x22;
+    static uint32_t         x23;
+    static uint32_t         x24;
+    static uint32_t         x25;
+    static uint32_t         x26;
+    static uint32_t         x27;
+    static uint32_t         x28;
+    static uint32_t         x29;
+    static uint32_t         x30;
+    static uint32_t         x31;
+}   resister_t;
 
 #endif
