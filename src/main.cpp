7cdfc8e72b2321602214a14be19c8f2decaa77c8
kohshi54 <kohshi54.yam@gmail.com>
2023-11-03 00:56:39 +0900

    increment pc by 4 bytes (32 bits).

--- src/main.cpp
+++ src/main.cpp
@@ -13,11 +13,10 @@ int main(int argc, char *argv[])
     std::cout << "hello from RV32I!" << std::endl;
 
     std::ifstream file(argv[1]);
-    std::string instr;
     instrmem.load_program(file);
-    while (regs.pc < instrmem.instr_memory.size())
+    while (regs.pc / 4 < instrmem.instr_memory.size())
     {
-        if (!decode(instrmem.instr_memory[regs.pc]))
+        if (!decode(instrmem.instr_memory[regs.pc / 4]))
         {
             std::cout << "exception occurred, abort." << std::endl;
             return (1);
@@ -26,7 +25,7 @@ int main(int argc, char *argv[])
         std::cout << regs.read("a5") << "<<<<<<<<<<<<" << std::endl;
         std::cout << regs.read("a4") << "<<<<<<<<<<<<" << std::endl;
         std::cout << "--------------" << std::endl;
-        regs.pc += 1; // modify this to 4 byte alligned later.
+        regs.pc += 4; // modify this to 4 byte alligned later. -> done.
     }
     regs.dump_registers();
     std::cout << regs.pc << std::endl;
